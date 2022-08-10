e3bd421d7a20b2a4711009dc9395bd49ad15e4f7
Ierultronic <amierulizzuddin21@gmail.com>
2022-08-10 16:01:09 +0800

    change about me to under construction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,13 +21,7 @@ function About() {
             alt="man-svgrepo"
           />
           <div className="AboutBio">
-            Hello! My name is <strong>Your Name</strong> Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged.
+            Hello! My name is <strong>Amierul Izzuddin</strong> and this website still inder construction. huhu...
           </div>
           <div className="AboutBio tagline2">
             I have become confident using the following technologies.
